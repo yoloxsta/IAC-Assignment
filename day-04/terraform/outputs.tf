f52b48f482b3000434dd949746b47fc28f0cff0d
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-06-28 08:13:54 +0630

    add tf

---
+++ day-04/terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.app.name
+}
