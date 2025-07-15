00a2870b6bc4ede4032ad5658f50058cca069836
yoloxsta <soetintaunghz10@gmail.com>
2025-07-15 11:37:01 +0630

    add conf

---
+++ day-10/www/html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World!</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>
+        <?php
+            echo 'We are running PHP, version: ' . phpversion();
+        ?>
+    </p>
+</body>
+</html>
